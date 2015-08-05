7d0454acca301e67f7db4be4b154223bf4b04000
Thomas Leskin <tleskin@gmail.com>
2015-08-04 20:17:31 -0600

    Create fixture, application adapter

--- app/models/idea.js
+++ app/models/idea.js
@@ -1,5 +1,31 @@
 import DS from 'ember-data';
 
-export default DS.Model.extend({
-
+let Idea = DS.Model.extend({
+  title: DS.attr('string'),
+  body: DS.attr('string')
 });
+
+Idea.reopenClass({
+  FIXTURES: [
+    {
+      { id: 1,
+        title: "First idea",
+        body: "Lorem ipsem..."
+      },
+      { id: 2,
+        title: "Second idea",
+        body: "Lorem ipsem..."
+      },
+      { id: 3,
+        title: "Third idea",
+        body: "Lorem ipsem..."
+      },
+      { id: 4,
+        title: "Forth idea",
+        body: "Lorem ipsem..."
+      }
+    }
+  ]
+});
+
+export default Idea;
