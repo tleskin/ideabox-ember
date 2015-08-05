a02adfde883d03cb8b7769ad808b1b913ab60f6e
Thomas Leskin <tleskin@gmail.com>
2015-08-04 21:06:34 -0600

    Get properties from form and add the idea to the store, save in DB

--- app/controllers/ideas.js
+++ app/controllers/ideas.js
@@ -2,5 +2,16 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 
-  
+  title: null,
+  body: null,
+
+  actions: {
+
+    addNewIdea: function () {
+      let idea = this.getProperties('title', 'body');
+      this.store.createRecord('idea', idea).save();
+    }
+
+  }
+
 });
