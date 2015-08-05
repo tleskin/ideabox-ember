849c66a2d8fb1cb4b9743c419829e453b2036a1a
Thomas Leskin <tleskin@gmail.com>
2015-08-04 20:22:12 -0600

    Fix fixture data

--- app/models/idea.js
+++ app/models/idea.js
@@ -7,23 +7,21 @@ let Idea = DS.Model.extend({
 
 Idea.reopenClass({
   FIXTURES: [
-    {
-      { id: 1,
-        title: "First idea",
-        body: "Lorem ipsem..."
-      },
-      { id: 2,
-        title: "Second idea",
-        body: "Lorem ipsem..."
-      },
-      { id: 3,
-        title: "Third idea",
-        body: "Lorem ipsem..."
-      },
-      { id: 4,
-        title: "Forth idea",
-        body: "Lorem ipsem..."
-      }
+    { id: 1,
+      title: "First idea",
+      body: "Lorem ipsem..."
+    },
+    { id: 2,
+      title: "Second idea",
+      body: "Lorem ipsem..."
+    },
+    { id: 3,
+      title: "Third idea",
+      body: "Lorem ipsem..."
+    },
+    { id: 4,
+      title: "Forth idea",
+      body: "Lorem ipsem..."
     }
   ]
 });
